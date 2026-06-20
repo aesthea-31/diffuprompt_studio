a444672a37b32cb9c1133a74815364b2513775d9
aesthea-31 <nrdpn6tyzt@privaterelay.appleid.com>
2026-06-20 22:39:11 +0900

    Add files via upload

---
+++ firebase_config.js
@@ -0,0 +1,9 @@
+// firebase_config.js
+export const firebaseConfig = {
+    apiKey: "NEXT_PUBLIC_FIREBASE_API_KEY",
+    authDomain: "diffuprompt.firebaseapp.com",
+    projectId: "diffuprompt",
+    storageBucket: "diffuprompt.firebasestorage.app",
+    messagingSenderId: "52137483632",
+    appId: "1:52137483632:web:1df98f0d20b6584c04c3b5" // 実際のappIdを記述
+};
